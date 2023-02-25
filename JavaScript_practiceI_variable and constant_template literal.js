f9e72d46d6aa7b6a335bb1dbf517b03f76463c1d
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 20:21:02 +0900

    Add files via upload

---
+++ JavaScript_practiceI_variable and constant_template literal.js
@@ -0,0 +1,9 @@
+const name = "Jane";
+const age = 14;
+
+// Please output "My name is Jane." to the console.
+console.log(`My name is ${name}.`) ;
+
+// Please output "I'm 14 now." to the console.
+console.log(`I'm ${age} now.`) ;
+
